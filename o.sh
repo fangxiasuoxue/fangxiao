381e80c3026fbf9e5ee896a58d3dcbf2c51aea4e
flextra <flextra@163.com>
2021-05-24 19:49:38 +0800

    2021-05-24 19:49:25-yb-加rpc.sh

--- o.sh
+++ o.sh
@@ -1,2 +1,71 @@
 #!/bin/sh
 echo 'lskaldk'
+echo "Shell 传递参数实例！";
+echo "执行的文件名：$0";
+echo "第一个参数为：$1";
+echo "第二个参数为：$2";
+echo "第三个参数为：$3";
+
+getLine(){
+    file=$1
+    section=$2
+    option=$3
+    tl=$(cat $file | wc -l)
+    lines=$(sed -n -e "/$section/=" $file)
+    tn=0
+    lindedata=''
+    for i in $lines
+    do
+        nlines=$(cat $file | awk -v i=$i '/^\[.*\]$/ {if(NR>i)print NR}')
+        nline=$(echo $nlines | awk '{print $1}')
+        if [ -n $nline ]
+        then
+            sl=$((i+1))
+            el=$((nline-1))
+            if [ $el -eq -1 ]
+            then
+                el=$tl
+            fi
+            #echo $tl $nline
+            if [ $((tl-nline)) -ge 0 ]
+            then
+                rs=$(cat $file | head -n $el | tail -n "+"$sl | grep -n '^'$option | tail -n 1)
+                #echo $rs
+                flag=$(awk -v a="$rs" -v b=":" 'BEGIN{print index(a,b)}')
+                if [ $flag -ne 0 ]
+                then
+                    px=$(echo $rs | awk -F ':' '{print $1}')
+                    tn=$((i+px))
+                fi
+            fi
+        fi
+    done
+    return $tn
+ 
+}
+getConfig(){
+    file=$1
+    section=$2
+    option=$3
+    getLine $file $section $option
+    line=$?
+    #echo $line
+    content=$(cat $file | awk '{if(NR=="'$line'"){print}}')
+    #echo $content
+    value=$(echo $content | awk -F '=' '{print $2}')
+    #echo $value
+ 
+}
+ 
+alterConfig(){
+    file=$1
+    section=$2
+    option=$3
+    rvalue=$4
+    getConfig $file $section $option
+    str=$line's/'$value'/'$rvalue'/'
+    sed -i "$str" $file
+}
+alterConfig $1 $2 $3 $4
+getConfig $1 $2 $3
+echo $line $value
---
+++ rpc.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+echo '开始安装*****'
+mkdir /tmp/log
+mkdir /usr/local/sevice
+cd /tmp/log
+yum install -y git wget
+# wget -O https://github.com/fatedier/frp/releases/download/v0.36.2/frp_0.36.2_linux_amd64.tar.gz
+wget -q -O - https://github.com/fatedier/frp/releases/download/v0.36.2/frp_0.36.2_linux_amd64.tar.gz | tar -zxf -
+cp frp_0.36.2_linux_amd64/frpc /usr/local/sevice/rpc
+chmod +x /usr/local/sevice/rpc
+
+echo '创建***目录成功'
+
+cat > /usr/local/sevice/rpc.ini << EOF
+[common]
+server_addr = $1
+server_port = $2
+
+[ssh]
+type = tcp
+local_ip = 127.0.0.1
+local_port = 22
+remote_port = $3
+EOF
+
+echo '配置生成1成功'
+
+# cp /tmp/log/fangxiao/rpc.ini /usr/local/sevice/rpc.ini
+# cp /tmp/log/fangxiao/sevice.service /etc/systemd/system/sevice.service
+
+cat > /etc/systemd/system/sevice.service << EOF
+[Unit]
+Description=Frp Client Service
+After=network.target
+
+[Service]
+Type=simple
+User=nobody
+Restart=on-failure
+RestartSec=5s
+ExecStart=/usr/local/sevice/rpc -c /usr/local/sevice/rpc.ini
+ExecReload=/usr/local/sevice/rpc reload -c /usr/local/sevice/rpc.ini
+
+[Install]
+WantedBy=multi-user.target
+EOF
+echo '配置生成2成功'
+
+systemctl enable sevice
+systemctl start sevice
+
+echo '启动成功'
